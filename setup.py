0bde345392fef5e28a6de4528566ffa245740b95
LongGang Pang <lgpang@qq.com>
2018-08-15 15:05:42 -0700

    update dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='jeteloss',  
-    version='0.5',   
+    version='0.6',   
     author = "Long-Gang Pang, Ya-Yun He and Xin-Nian Wang",
     author_email = "lgpang@qq.com, heyayun@gmail.com, xnwang@lbl.gov",
     description = "Data-driven extraction of jet energy loss distributions in heavy-ion collisions",
@@ -18,10 +18,12 @@ setup(
     exclude_package_date={'':['.gitignore']},
     install_requires=[
         'numpy>=1.14',
-        'pymc>=2.3.6',
         'h5py>=2.8.0',
+        'scipy>=1.1.0',
         'matplotlib>=2.2.0',
-        'scipy>=1.1.0'
+        'pandas>=0.23.3',
+        'seaborn>=0.7.1',
+        'pymc>=2.3.6',
     ],
     license = "MIT",
     keywords = "Bayesian, MCMC, Jet energy loss extractor, RAA",
